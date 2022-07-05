4314b01f49379d72195c32f4ce3a2de1b1acb125
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-07-05 22:22:56 +0300

    changed App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 
-import './App.css';
+import { Calendar } from './components';
+import { formatDate } from './utils/helpers/date';
 
-function App() {
+import './static/css/global.css';
+
+export const App: React.FC = () => {
+  const [selectedDate, setSelectedDay] = React.useState(new Date());
+
   return (
-    <div className="App">
-      hello
+    <div className='app__container'>
+      <div className='date__container'>{formatDate(selectedDate, 'DDD DD MMM YYYY')}</div>
 
+      <Calendar selectedDate={selectedDate} selectDate={(date) => setSelectedDay(date)} />
     </div>
   );
-}
+};
 
 export default App;
