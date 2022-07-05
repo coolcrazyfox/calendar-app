a5451cf17e35a8b465c507c7738e2ed7c7aa36d0
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-07-05 23:25:11 +0300

    changed App and add parallax

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
+import Tilt from 'react-parallax-tilt';
 
-import { Calendar } from './components';
-import { formatDate } from './utils/helpers/date';
+import {Calendar} from './components';
+import {formatDate} from './utils/helpers/date';
 
 import './static/css/global.css';
 
 export const App: React.FC = () => {
-  const [selectedDate, setSelectedDay] = React.useState(new Date());
+    const [selectedDate, setSelectedDay] = React.useState(new Date());
 
-  return (
-    <div className='app__container'>
-      <div className='date__container'>{formatDate(selectedDate, 'DDD DD MMM YYYY')}</div>
+    return (
 
-      <Calendar selectedDate={selectedDate} selectDate={(date) => setSelectedDay(date)} />
-    </div>
-  );
+        <div className='app__container'>
+            <Tilt>
+                <div className='date__container'>{formatDate(selectedDate, 'DDD DD MMM YYYY')}</div>
+
+                <Calendar selectedDate={selectedDate} selectDate={(date) => setSelectedDay(date)}/>
+            </Tilt>
+        </div>
+
+    );
 };
 
 export default App;
